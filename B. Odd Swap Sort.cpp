5c2d72f21e919d3bdb2bad915c27b7786e36f7a9
Mustain Murtaza Taib <65018690+taib0101@users.noreply.github.com>
2023-06-14 22:32:08 +0600

    Update B. Odd Swap Sort.cpp

--- B. Odd Swap Sort.cpp
+++ B. Odd Swap Sort.cpp
@@ -55,3 +55,70 @@ int main()
     }
     return 0;
 }
+
+////////////////////////
+//Solved with OOP
+#include<bits/stdc++.h>
+using namespace std;
+
+class mainn
+{
+    public:
+        vector < int > odd,even;// member variable
+        bool check(int arr[])//member function and inline member function
+        {
+            int os = odd.size()-1;
+            int es = even.size()-1;
+            for(int i = 0; i < os; ++i)
+            {
+                if(arr[odd[i]] > arr[odd[i+1]])
+                    return 0;
+            }
+            for(int i = 0; i < es; ++i)
+            {
+                if(arr[even[i]] > arr[even[i+1]])
+                    return 0;
+            }
+            return 1;
+        }
+        void odd_oop()
+        {   
+            int t;
+            cin >> t;
+            while(t--)
+            {
+                    int n;
+                    cin >> n;
+                    int arr[n+10];
+                    odd.clear(),even.clear();
+                    bool bl = 1;
+                    for(int i = 0; i < n; ++i)
+                    {
+                        scanf("%d",&arr[i]);
+                        if(i != 0)
+                        {
+                            if(arr[i-1] > arr[i])
+                                bl = 0;
+                        }
+                        (arr[i]%2) ? odd.push_back(i):even.push_back(i);
+                    }
+                    if(bl != 1)
+                    {
+                        if(check(arr))
+                            cout << "Yes" << endl;
+                        else
+                            cout << "No" << endl;
+                    }
+                    else
+                        cout << "Yes" << endl;
+            }
+        }
+};
+int main()
+{
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+    mainn cls;
+    cls.odd_oop();
+    return 0;
+}
